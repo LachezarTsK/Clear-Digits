e8debf5760a5e174b24a94704d5a003069eb88b0
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-06-27 21:05:48 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,24 @@
+
+/**
+ * @param {string} input
+ * @return {string}
+ */
+var clearDigits = function (input) {
+    const clearDigits = new Array();
+    for (let current of input) {
+        if (isDigit(current) && clearDigits.length > 0) {
+            clearDigits.pop();
+            continue;
+        }
+        clearDigits.push(current);
+    }
+    return clearDigits.join('');
+};
+
+/**
+ * @param {string} char
+ * @return {boolean}
+ */
+function isDigit(char) {
+    return char >= '0' && char <= '9';
+}
