e12f83adb1a938ac1ff417a08507507ccc98083c
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-06-27 21:10:28 +0200

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,20 @@
+
+using System;
+
+public class Solution
+{
+    public string ClearDigits(string input)
+    {
+        StringBuilder clearDigits = new StringBuilder();
+        foreach (char current in input)
+        {
+            if (Char.IsDigit(current) && clearDigits.Length > 0)
+            {
+                clearDigits.Remove(clearDigits.Length - 1, 1);
+                continue;
+            }
+            clearDigits.Append(current);
+        }
+        return clearDigits.ToString();
+    }
+}
